6f6262f03c0f8d2348e290db475346b6c1420397
Ural <ural2001@gmail.com>
2020-04-24 17:32:06 +0300

    Created the blog, the account and the qa app
    Also added a UserProfile model into account app

---
+++ account/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+from image_cropping import ImageCroppingMixin
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    fields = ['user', 'photo', ]
---
+++ account/models.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.db import models
+from image_cropping import ImageRatioField
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='profile')
+    photo = models.ImageField(upload_to='account/avatars/%Y/%m/%d/', blank=True)
+    avatar = ImageRatioField('photo', '106x100')
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
