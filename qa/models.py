ac5dbfb1bbe1158b5069cbfe61c49ab366261437
Ural <ural2001@gmail.com>
2020-05-16 16:09:12 +0300

    Create Question and Comment models

--- qa/models.py
+++ qa/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from account.models import UserProfile
 
 # Create your models here.
 
@@ -6,3 +7,14 @@ from django.db import models
 class Theme(models.Model):
     name = models.CharField(max_length=100)
 
+
+class Comment(models.Model):
+    text = models.TextField(max_length=400)
+    author = models.ForeignKey(UserProfile, related_name='comments', on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField(max_length=400)
+    author = models.ForeignKey(UserProfile, related_name='questions', on_delete=models.CASCADE)
+    comments = models.ManyToManyField(Comment, blank=True, related_name='question')
