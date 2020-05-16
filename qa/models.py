9832325d920008428f778025132bed86aca09d6c
Ural <ural2001@gmail.com>
2020-05-16 16:18:42 +0300

    Rename Comment model into Answer

--- qa/models.py
+++ qa/models.py
@@ -8,7 +8,7 @@ class Theme(models.Model):
     name = models.CharField(max_length=100)
 
 
-class Comment(models.Model):
+class Answer(models.Model):
     text = models.TextField(max_length=400)
     author = models.ForeignKey(UserProfile, related_name='comments', on_delete=models.CASCADE)
 
@@ -17,4 +17,4 @@ class Question(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=400)
     author = models.ForeignKey(UserProfile, related_name='questions', on_delete=models.CASCADE)
-    comments = models.ManyToManyField(Comment, blank=True, related_name='question')
+    answers = models.ManyToManyField(Answer, blank=True, related_name='question')
