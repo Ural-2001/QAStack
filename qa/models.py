89239d2f63b9cac8b2cb31acbd7ed40a80650943
Ural <ural2001@gmail.com>
2020-05-16 16:25:49 +0300

    Edit Question and Answer models

--- qa/models.py
+++ qa/models.py
@@ -8,13 +8,16 @@ class Theme(models.Model):
     name = models.CharField(max_length=100)
 
 
-class Answer(models.Model):
-    text = models.TextField(max_length=400)
-    author = models.ForeignKey(UserProfile, related_name='comments', on_delete=models.CASCADE)
-
-
 class Question(models.Model):
+    theme = models.ForeignKey(Theme, related_name='questions', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=400)
     author = models.ForeignKey(UserProfile, related_name='questions', on_delete=models.CASCADE)
-    answers = models.ManyToManyField(Answer, blank=True, related_name='question')
+    # answers = models.ManyToManyField(Answer, blank=True, related_name='question')
+
+
+class Answer(models.Model):
+    text = models.TextField(max_length=400)
+    author = models.ForeignKey(UserProfile, related_name='comments', on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, blank=True, related_name='question')
+
