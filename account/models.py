a20480614f387d25d4f2a59b3a08a662a4d30ced
Ural <ural2001@gmail.com>
2020-05-17 20:05:24 +0300

    Create Bucket model

--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from image_cropping import ImageRatioField
+from blog.models import Post
 
 
 class UserProfile(models.Model):
@@ -16,3 +17,8 @@ class UserProfile(models.Model):
 class Subscription(models.Model):
     who = models.ForeignKey(UserProfile, related_name='subscriptions', on_delete=models.CASCADE)
     on_whom = models.ForeignKey(UserProfile, related_name='subscribers', on_delete=models.CASCADE)
+
+
+class Bucket(models.Model):
+    saved_post = models.ForeignKey(Post, related_name='posts', on_delete=models.CASCADE)
+
